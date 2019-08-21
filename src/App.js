c6e06abbd27bf5ca900245b15e15757817f4c95f
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-08-21 23:31:01 +0300

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 
-class App extends React.Component {
-  state = {
+const App = () => {
+  const [currentState, updateState] = useState({
     persons: [
       { name: 'Max', age: 29 },
       { name: 'Dani', age: 18 },
       { name: 'Tanya', age: 21 },
-    ]
-  }
+    ],
+    title: 'State'
+  });
 
-  addYears = () => {
-    let newPersonsYears = this.state.persons.map( person => {
+  const addYears = () => {
+    let persons = currentState.persons.map( person => {
       let {name, age} = person;
       age++;
       return ({name, age})
     });
-    this.setState({
-      persons: newPersonsYears
-    });
+    updateState({...currentState, persons});
+    console.log(currentState);
   }
 
-  render() {
-    return (
-      <div>
-        <h1>This is some users:</h1>
-        <button onClick={this.addYears}>Add years</button>
-        {this.state.persons.map(person => <Person name={person.name} age={person.age} />)}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>This is some users:</h1>
+      <button onClick={addYears}>Add years</button>
+      {currentState.persons.map(person => <Person name={person.name} age={person.age} />)}
+    </div>
+  )
 };
 
 export default App;
