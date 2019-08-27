5ab6f433a8275e8dffc4a8e7101e1b749e851ef4
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-08-28 00:08:37 +0300

    added radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Person from './Person/Person';
+import './style.css';
+import Radium, { StyleRoot } from 'radium';
 
 // const App = () => {
 //   const [currentState, updateState] = useState({
@@ -78,27 +80,59 @@ class App extends React.Component {
     const style = {
       color: `red`,
     }
-    const persons = <div>
-      {this.state.persons.map((person, index) => (
-        <Person
-          change={(event) => this.change(event, person.id)}
-          // click={() => this.removePerson(index)}
-          key={index}
-          name={person.name}
-          age={person.age}
-        />))}
-    </div>;
+    const btnStyle = {
+      color: 'white',
+      backgroundColor: 'green',
+      padding: '6px 9px',
+      ':hover': {
+        border: '3px solid #ccc',
+      }
+    }
+
+    let persons;
+    if (this.state.showPerson) {
+      persons = <div>
+        {this.state.persons.map((person, index) => (
+          <Person
+            change={(event) => this.change(event, person.id)}
+            click={() => this.removePerson(index)}
+            key={index}
+            name={person.name}
+            age={person.age}
+          />))}
+      </div>;
+      btnStyle.backgroundColor = 'red';
+    } else {
+      persons = null;
+      btnStyle.backgroundColor = 'green';
+      btnStyle[':hover'] = {borderRadius: '12px'}
+    }
+
+    const classes = [];
+    if(this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
 
     return (
-      <div>
-        <h1 style={style}>This is some users:</h1>
-        <button onClick={this.tooglePerson}>Add years</button>
-        { this.state.showPerson === true ?
-         persons: null
-        }
-      </div>
+      <StyleRoot>
+        <div>
+          <h1 style={style}>This is some users:</h1>
+          <p className={classes.join(' ')}>Some text</p>
+          <button
+            style={btnStyle}
+            onClick={this.tooglePerson}>
+              Add years
+          </button>
+          { this.state.showPerson === true ?
+           persons : null
+          }
+        </div>
+      </StyleRoot>
     )
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 
 const Person = ({name, age, children, click, change}) => {
+  const style = {
+    '@media (max-width: 500px)': {
+      width: '90%',
+    }
+  }
   return (
-    <div className="Person" onClick={click}>
+    <div style={style} className="Person" onClick={click}>
+      <button
+          onClick={click}>
+          x
+        </button>
       <p>I'm a {name}. I'm {age} y.o. </p>
       <p>{children}</p>
       <input type="text" value={name} onChange={change}/>
@@ -11,4 +21,4 @@ const Person = ({name, age, children, click, change}) => {
     )
 }
 
-export default Person;
+export default Radium(Person);
