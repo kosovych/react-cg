8eb15ec9fcb0776fcdc589950c76b83a89ae8d14
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-12-13 09:56:39 +0200

    added menu

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../../static/img/logo/logo.svg'
+
+const Logo = () => (
+  <img src={logo} alt="Burger Logo"/>
+);
+
+export default Logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,33 +1,40 @@
 import React, { useState } from 'react';
+import Logo from '../../Logo/Logo';
+import Backdrop from '../../../components/ui/Backdrop/Backdrop';
 
 const Toolbar = () => {
   const [ isOpen, setOpen ] = useState(false);
   return (
-    <header className="fixed-top">
-      <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#e3f2fd'}}>
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button onClick={ ()=> setOpen(!isOpen) } className="navbar-toggler" type="button" >
-          <span className="navbar-toggler-icon" />
-        </button>
+    <>
+      <header className="fixed-top">
+        <div className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#e3f2fd'}}>
+          <a className="navbar-brand" href="#">
+            <Logo />
+          </a>
+          <button onClick={ ()=> setOpen(!isOpen) } className="navbar-toggler" type="button" >
+            <span className="navbar-toggler-icon" />
+          </button>
 
-        <div className={`${isOpen ? 'show' : ''} collapse navbar-collapse`} id="navbarColor03">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-          </ul>
+          <nav className={`${isOpen ? 'navbar-collapse--open' : ''} collapse navbar-collapse`}>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </nav>
-    </header>
+      </header>
+      {isOpen && <Backdrop clicked={() => setOpen(!isOpen)} /> }
+    </>
   )
 };
 
